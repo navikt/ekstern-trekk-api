dfd757f8692b271237b0bab55535a577faf588f2
aage-NAV <48911967+aage-NAV@users.noreply.github.com>
2026-05-11 11:30:26 +0200

    Fikser på DB, MQ-oppsett, auth (#7)
    * La til db-kolonne org_nr
    
    * Fjernet PUT-endepunkt med angitt ID
    
    * Update src/main/kotlin/no/nav/trekkapi/innmelding/ResponseReceiver.kt
    
    Co-authored-by: Thomas Burnett <7643677+thburnett@users.noreply.github.com>
    
    * La til accesspolicy for MQ, mer MQ-logging, og en mergefiks
    
    * Tar bort ekstra MQ-logging igjen
    
    * La på maskinporten auth igjen, med litt debuglogging
    
    * Mergetrøbbel
    
    ---------
    
    Co-authored-by: Thomas Burnett <7643677+thburnett@users.noreply.github.com>

--- src/main/kotlin/no/nav/trekkapi/auth/MaskinportenAuthConfig.kt
+++ src/main/kotlin/no/nav/trekkapi/auth/MaskinportenAuthConfig.kt
@@ -7,6 +7,7 @@ import no.nav.security.token.support.v3.IssuerConfig
 import no.nav.security.token.support.v3.RequiredClaims
 import no.nav.security.token.support.v3.TokenSupportConfig
 import no.nav.security.token.support.v3.TokenValidationContextPrincipal
+import no.nav.trekkapi.log
 
 // Lag en config for hver tjenestetype, hvis det blir flere tjenester (sjekker forskjellig audience for hver)
 const val MASKINPORTEN_AUTH_INMMELDING = "MASKINPORTEN_INNMELDING"
@@ -19,7 +20,8 @@ const val SCOPE_INNMELDING = "nav:utbetaling/trekkopplysning/innmelding"
 fun getInnmeldingConfig(): TokenSupportConfig = TokenSupportConfig(
     IssuerConfig(
         name = MASKINPORTEN_AUTH_INMMELDING,
-        discoveryUrl = getEnvVar("MASKINPORTEN_WELL_KNOWN_URL", "https://test.maskinporten.no/.well-known/oauth-authorization-server"),
+        // "https://test.maskinporten.no/.well-known/oauth-authorization-server"
+        discoveryUrl = getEnvVar("MASKINPORTEN_WELL_KNOWN_URL", "http://localhost:3344/AZURE_AD/.well-known/openid-configuration"),
         acceptedAudience = listOf(SCOPE_INNMELDING),
         optionalClaims = listOf("aud", "sub")
     )
@@ -29,10 +31,18 @@ fun getRequiredClaims(): RequiredClaims = RequiredClaims(issuer = MASKINPORTEN_A
 
 suspend fun RoutingContext.orgNrFromTokenValidationContext(): String? {
     val principal = call.principal<TokenValidationContextPrincipal>()
+    log.debug("### Principal: $principal")
     val context = principal?.context
+    log.debug("### Context: $context")
     val claims = context?.getClaims(MASKINPORTEN_AUTH_INMMELDING)
+    log.debug("### Claims for $MASKINPORTEN_AUTH_INMMELDING: $claims")
     val consumer = claims?.get("consumer") as Map<*, *>
-    return consumer.extractOrgnummer()
+    log.debug("### Claim 'consumer': $consumer")
+    val sub = claims?.get("sub") as Map<*, *>
+    log.debug("### Claim 'sub': $sub")
+    val orgnr = consumer.extractOrgnummer()
+    log.debug("### Extracted orgnr: $orgnr")
+    return orgnr
 }
 
 // Se format under
--- src/main/kotlin/no/nav/trekkapi/plugin/Routes.kt
+++ src/main/kotlin/no/nav/trekkapi/plugin/Routes.kt
@@ -1,11 +1,14 @@
 package no.nav.trekkapi.plugin
 
 import io.ktor.server.application.Application
+import io.ktor.server.auth.authenticate
 import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.routing
 import io.micrometer.prometheus.PrometheusMeterRegistry
+import no.nav.emottak.utils.environment.isProdEnv
+import no.nav.trekkapi.auth.MASKINPORTEN_AUTH_INMMELDING
 import no.nav.trekkapi.innmelding.TrekkInnmeldingService
 import no.nav.trekkapi.innmelding.innmeldingRoutes
 import no.nav.trekkapi.innmelding.testRoutes
@@ -19,10 +22,12 @@ fun Application.configureRoutes(
         get("/") {
             call.respondText("Ekstern-trekk-api running properly")
         }
+        authenticate(MASKINPORTEN_AUTH_INMMELDING) {
+            innmeldingRoutes(trekkInnmeldingService)
+        }
+        if (!isProdEnv()) {
+            testRoutes(trekkInnmeldingService)
+        }
         swaggerUI(path = "swagger", swaggerFile = "openapi/documentation.yaml")
-//        authenticate(MASKINPORTEN_AUTH_INMMELDING) { HANGS forever ?
-        innmeldingRoutes(trekkInnmeldingService)
-//        }
-        testRoutes(trekkInnmeldingService)
     }
 }
