6193fc921cf7249f7e2a71f7311f082a7e52f9fc
Thomas Burnett <thburnett@users.noreply.github.com>
2026-05-13 09:29:47 +0200

    Fikser oppdaterte micrometer imports

--- src/main/kotlin/no/nav/trekkapi/App.kt
+++ src/main/kotlin/no/nav/trekkapi/App.kt
@@ -7,8 +7,8 @@ import arrow.fx.coroutines.ResourceScope
 import arrow.fx.coroutines.resourceScope
 import io.ktor.server.application.Application
 import io.ktor.server.netty.Netty
-import io.micrometer.prometheus.PrometheusConfig
-import io.micrometer.prometheus.PrometheusMeterRegistry
+import io.micrometer.prometheusmetrics.PrometheusConfig
+import io.micrometer.prometheusmetrics.PrometheusMeterRegistry
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.launch
