25f9db5a72f7444c9772ea8a7672e132b66452fe
Thomas Burnett <thburnett@users.noreply.github.com>
2026-05-18 14:49:45 +0200

    Bump ktor test og plugin i tillegg

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm") version "2.3.21"
-    id("io.ktor.plugin") version "3.4.3"
+    id("io.ktor.plugin") version "3.5.0"
     kotlin("plugin.serialization") version "2.3.21"
     id("org.jlleitschuh.gradle.ktlint") version "14.2.0"
 }
