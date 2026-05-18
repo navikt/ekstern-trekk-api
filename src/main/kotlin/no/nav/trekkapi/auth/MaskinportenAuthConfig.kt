af75193346f69f1b5a262e4fc9a07727632b3c12
RettIProd <85939620+RettIProd@users.noreply.github.com>
2026-05-18 14:53:00 +0200

    Optional claims (#29)

--- src/main/kotlin/no/nav/trekkapi/auth/MaskinportenAuthConfig.kt
+++ src/main/kotlin/no/nav/trekkapi/auth/MaskinportenAuthConfig.kt
@@ -24,7 +24,7 @@ fun getInnmeldingConfig(): TokenSupportConfig =
             // "https://test.maskinporten.no/.well-known/oauth-authorization-server"
             discoveryUrl = getEnvVar("MASKINPORTEN_WELL_KNOWN_URL", "http://localhost:3344/AZURE_AD/.well-known/openid-configuration"),
             acceptedAudience = listOf(SCOPE_INNMELDING),
-            // optionalClaims = listOf("aud", "sub"),
+            optionalClaims = listOf("aud", "sub"),
         ),
     )
 
